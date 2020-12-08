15bdda30c60f169deb0186e44cd582197e4d9d10
Stanislav-Stashevskiy <75270781+Stanislav-Stashevskiy@users.noreply.github.com>
2020-12-08 10:52:12 +0300

    Add files via upload

--- auto_clicker.py
+++ auto_clicker.py
@@ -1,5 +1,10 @@
 import pyautogui as pg
+import time
 
-while True:
+quantity = int(input('введите кол-во кликов:'))
+print('у вас 10 секунд чтобы навести мышь на место кликов!')
+time.sleep(10)
+
+for i in range(quantity):
     pos = pg.position()
     pg.click(pos)
